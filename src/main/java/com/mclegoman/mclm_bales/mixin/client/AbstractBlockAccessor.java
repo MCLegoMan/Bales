48a3105ffdcbd5c69951754f9ba34d5741cdc5e8
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-09-09 18:37:38 +1200

    1.0 - sounds now mimic replacement block

---
+++ src/main/java/com/mclegoman/mclm_bales/mixin/client/AbstractBlockAccessor.java
@@ -0,0 +1,20 @@
+/*
+    Bales
+    Contributor(s): MCLegoMan
+    Github: https://github.com/MCLegoMan/Bales
+    Licence: GNU LGPLv3
+*/
+
+package com.mclegoman.mclm_bales.mixin.client;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.sound.BlockSoundGroup;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(AbstractBlock.class)
+public interface AbstractBlockAccessor {
+	@Invoker("getSoundGroup")
+	BlockSoundGroup invokeGetSoundGroup(BlockState state);
+}
---
+++ src/main/java/com/mclegoman/mclm_bales/mixin/client/BlockMixin.java
@@ -0,0 +1,28 @@
+/*
+    Bales
+    Contributor(s): MCLegoMan
+    Github: https://github.com/MCLegoMan/Bales
+    Licence: GNU LGPLv3
+*/
+
+package com.mclegoman.mclm_bales.mixin.client;
+
+import com.mclegoman.mclm_bales.config.BalesConfig;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.sound.BlockSoundGroup;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractBlock.class)
+public abstract class BlockMixin {
+	@Shadow protected abstract Block asBlock();
+	@Inject(at = @At(value = "HEAD"), method = "getSoundGroup", cancellable = true)
+	private void loafy$getSoundGroup(BlockState state, CallbackInfoReturnable<BlockSoundGroup> cir) {
+		if (!this.asBlock().getDefaultState().equals(BalesConfig.getBlockState())) cir.setReturnValue(((AbstractBlockAccessor)BalesConfig.getBlockState().getBlock()).invokeGetSoundGroup(BalesConfig.getBlockState()));
+	}
+}
